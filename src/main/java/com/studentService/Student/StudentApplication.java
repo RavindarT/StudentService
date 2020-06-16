982b84782befb0324143efe0f8a5682c3166b96e
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-06-16 12:19:23 -0400

    StudentService

---
+++ src/main/java/com/studentService/Student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.studentService.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
